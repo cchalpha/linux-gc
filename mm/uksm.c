c6eba34f8a607e577cd5d1087588ff44c408f718
Nai Xia <nai.xia@gmail.com>
2012-05-05 21:12:43 +0800

    UKSM: Documentation and some comment fix.

--- mm/uksm.c
+++ mm/uksm.c
@@ -9,29 +9,25 @@
  *      It automatically scans all user processes' anonymous VMAs. Kernel-user
  *      interaction to submit a memory area to KSM is no longer needed.
  *
- * 2. Rich area detection based on random sampling:
+ * 2. Rich area detection:
  *      It automatically detects rich areas containing abundant duplicated
- *      pages based on their randomly-sampled history. Rich areas are given
- *      a full scan speed. Poor areas are sampled at a reasonable speed with
- *      very low CPU consumption.
+ *      pages based. Rich areas are given a full scan speed. Poor areas are
+ *      sampled at a reasonable speed with very low CPU consumption.
  *
- * 3. Per-page scan speed improvement:
- *      A new hash algorithm(random_sample_hash) is proposed. Quite usually,
- *      it's enough to distinguish pages by hashing their partial content
- *      instead of full pages. This algorithm can automatically adapt to this
- *      situation. For the best case, only one 32-bit-word/page is needed to
- *      get the hash value for distinguishing pages. For the worst case, it's as
- *      fast as SuperFastHash.
+ * 3. Ultra Per-page scan speed improvement:
+ *      A new hash algorithm is proposed. As a result, on a machine with
+ *      Core(TM)2 Quad Q9300 CPU in 32-bit mode and 800MHZ DDR2 main memory, it
+ *      can scan memory areas that does not contain duplicated pages at speed of
+ *      627MB/sec ~ 2445MB/sec and can merge duplicated areas at speed of
+ *      477MB/sec ~ 923MB/sec.
  *
  * 4. Thrashing area avoidance:
  *      Thrashing area(an VMA that has frequent Ksm page break-out) can be
  *      filtered out. My benchmark shows it's more efficient than KSM's per-page
  *      hash value based volatile page detection.
  *
- * 5. Hash-value-based identical page detection:
- *      It no longer uses "memcmp" based page detection any more.
  *
- * 6. Misc changes upon KSM:
+ * 5. Misc changes upon KSM:
  *      * It has a fully x86-opitmized memcmp dedicated for 4-byte-aligned page
  *        comparison. It's much faster than default C version on x86.
  *      * rmap_item now has an struct *page member to loosely cache a
@@ -41,7 +37,7 @@
  *      * try_to_merge_two_pages() now can revert a pte if it fails. No break_
  *        ksm is needed for this case.
  *
- * 7. Full Zero Page consideration(contributed by Figo Zhang)
+ * 6. Full Zero Page consideration(contributed by Figo Zhang)
  *    Now uksmd consider full zero pages as special pages and merge them to an
  *    special unswappable uksm zero page.
  */
