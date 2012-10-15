e975b62eb712b2c8de2f731479dfb6de51452aa5
Alfred Chen <cchalpha@gmail.com>
2012-10-15 10:46:00 +0800

    The Brain Fuck Scheduler v0.424 by Con Kolivas.
    A single shared runqueue O(n) strict fairness earliest deadline first design.
    
    Excellent throughput and latency for 1 to many CPUs on desktop and server
    commodity hardware.
    Not recommended for 4096 cpus.
    
    Scalability is optimal when your workload is equal to the number of CPUs on
    bfs. ie you should ONLY do make -j4 on quad core, -j2 on dual core and so on.
    
    Features SCHED_IDLEPRIO and SCHED_ISO scheduling policies as well.
    You do NOT need to use these policies for good performance, they are purely
    optional for even better performance in extreme conditions.
    
    To run something idleprio, use schedtool like so:
    
    schedtool -D -e make -j4
    
    To run something isoprio, use schedtool like so:
    
    schedtool -I -e amarok
    
    Includes accurate sub-tick accounting of tasks so userspace reported
    cpu usage may be very different if you have very short lived tasks.
    
    -ck

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -28,8 +28,8 @@
  * It helps to keep variable names smaller, simpler
  */
 
-#define DEF_FREQUENCY_DOWN_DIFFERENTIAL		(10)
-#define DEF_FREQUENCY_UP_THRESHOLD		(80)
+#define DEF_FREQUENCY_DOWN_DIFFERENTIAL		(26)
+#define DEF_FREQUENCY_UP_THRESHOLD		(63)
 #define DEF_SAMPLING_DOWN_FACTOR		(1)
 #define MAX_SAMPLING_DOWN_FACTOR		(100000)
 #define MICRO_FREQUENCY_DOWN_DIFFERENTIAL	(3)
@@ -472,10 +472,10 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 
 	/*
 	 * Every sampling_rate, we check, if current idle time is less
-	 * than 20% (default), then we try to increase frequency
+	 * than 37% (default), then we try to increase frequency
 	 * Every sampling_rate, we look for a the lowest
 	 * frequency which can sustain the load while keeping idle time over
-	 * 30%. If such a frequency exist, we try to decrease to this frequency.
+	 * 63%. If such a frequency exist, we try to decrease to this frequency.
 	 *
 	 * Any frequency increase takes it to the maximum frequency.
 	 * Frequency reduction happens at minimum steps of
