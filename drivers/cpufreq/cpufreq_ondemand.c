b2912adfc8af58528e5a9d846e4873c1caa67331
Alfred Chen <cchalpha@gmail.com>
2013-09-10 11:16:53 +0800

    The Brain Fuck Scheduler v0.441 by Con Kolivas.
    A single shared runqueue O(n) strict fairness earliest deadline first design.
    
    Excellent throughput and latency for 1 to many CPUs on desktop and server
    commodity hardware.
    Not recommended for 4096 cpus.
    
    Scalability is optimal when your workload is equal to the number of CPUs on
    bfs. ie you should ONLY do make -j4 on quad core, -j2 on dual core and so on.
    
    Features SCHED_IDLEPRIO and SCHED_ISO scheduling policies as well.
    You do NOT need to use these policies for good performance, they are purely
    optional for even better performance in extreme conditions.
    
    To run something idleprio, use schedtool like so:
    
    schedtool -D -e make -j4
    
    To run something isoprio, use schedtool like so:
    
    schedtool -I -e amarok
    
    Includes accurate sub-tick accounting of tasks so userspace reported
    cpu usage may be very different if you have very short lived tasks.
    
    -ck
    
    Conflicts:
    	drivers/cpufreq/cpufreq.c
    	drivers/cpufreq/cpufreq_ondemand.c
    	include/linux/sched.h

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -19,7 +19,8 @@
 #include "cpufreq_governor.h"
 
 /* On-demand governor macros */
-#define DEF_FREQUENCY_UP_THRESHOLD		(80)
+#define DEF_FREQUENCY_DOWN_DIFFERENTIAL		(26)
+#define DEF_FREQUENCY_UP_THRESHOLD		(63)
 #define DEF_SAMPLING_DOWN_FACTOR		(1)
 #define MAX_SAMPLING_DOWN_FACTOR		(100000)
 #define MICRO_FREQUENCY_UP_THRESHOLD		(95)
@@ -148,9 +149,13 @@ static void dbs_freq_increase(struct cpufreq_policy *policy, unsigned int freq)
 }
 
 /*
- * Every sampling_rate, we check, if current idle time is less than 20%
- * (default), then we try to increase frequency. Else, we adjust the frequency
- * proportional to load.
+ * Every sampling_rate, we check, if current idle time is less than 37%
+ * (default), then we try to increase frequency. Every sampling_rate, we look
+ * for the lowest frequency which can sustain the load while keeping idle time
+ * over 63%. If such a frequency exist, we try to decrease to this frequency.
+ *
+ * Any frequency increase takes it to the maximum frequency. Frequency reduction
+ * happens at minimum steps of 5% (default) of current frequency
  */
 static void od_check_cpu(int cpu, unsigned int load)
 {
