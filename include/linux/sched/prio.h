6af40417e00365d4f85967f5fd9be56e3a9813d4
Alfred Chen <cchalpha@gmail.com>
2014-05-07 12:22:33 +0800

    The Brain Fuck Scheduler v0.447 by Con Kolivas.
    A single shared runqueue O(n) strict fairness earliest deadline first design.
    
    Excellent throughput and latency for 1 to many CPUs on desktop and server
    commodity hardware.
    Not recommended for 4096 cpus.
    
    Scalability is optimal when your workload is equal to the number of CPUs on
    bfs. ie you should ONLY do make -j4 on quad core, -j2 on dual core and so on.
    
    Features SCHED_IDLEPRIO and SCHED_ISO scheduling policies as well.
    You do NOT need to use these policies for good performance, they are purely
    optional for even better performance in extreme conditions.
    
    To run something idleprio, use schedtool like so:
    
    schedtool -D -e make -j4
    
    To run something isoprio, use schedtool like so:
    
    schedtool -I -e amarok
    
    Includes accurate sub-tick accounting of tasks so userspace reported
    cpu usage may be very different if you have very short lived tasks.
    
    -ck
    
    Conflicts:
    	include/linux/sched/rt.h
    	kernel/sched/Makefile
    	lib/Kconfig.debug

--- include/linux/sched/prio.h
+++ include/linux/sched/prio.h
@@ -18,11 +18,24 @@
  * MAX_RT_PRIO must not be smaller than MAX_USER_RT_PRIO.
  */
 
+#ifdef CONFIG_SCHED_BFS
+#define MAX_USER_RT_PRIO	100
+#define MAX_RT_PRIO		(MAX_USER_RT_PRIO + 1)
+#define DEFAULT_PRIO		(MAX_RT_PRIO + 20)
+
+#define PRIO_RANGE		(40)
+#define MAX_PRIO		(MAX_RT_PRIO + PRIO_RANGE)
+#define ISO_PRIO		(MAX_RT_PRIO)
+#define NORMAL_PRIO		(MAX_RT_PRIO + 1)
+#define IDLE_PRIO		(MAX_RT_PRIO + 2)
+#define PRIO_LIMIT		((IDLE_PRIO) + 1)
+#else /* CONFIG_SCHED_BFS */
 #define MAX_USER_RT_PRIO	100
 #define MAX_RT_PRIO		MAX_USER_RT_PRIO
 
 #define MAX_PRIO		(MAX_RT_PRIO + NICE_WIDTH)
 #define DEFAULT_PRIO		(MAX_RT_PRIO + NICE_WIDTH / 2)
+#endif /* CONFIG_SCHED_BFS */
 
 /*
  * Convert user-nice values [ -20 ... 0 ... 19 ]
