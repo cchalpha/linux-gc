43a7dad404a89640378ae48186f6925f21c4aa9c
Alfred Chen <cchalpha@gmail.com>
2014-07-03 23:50:12 +0800

    [BFS] Fix goto unlock in get_nohz_timer_target()

--- kernel/sched/bfs.c
+++ kernel/sched/bfs.c
@@ -5030,9 +5030,10 @@ int get_nohz_timer_target(int pinned)
 	rcu_read_lock();
 	for_each_domain(cpu, sd) {
 		for_each_cpu(i, sched_domain_span(sd)) {
-			if (!idle_cpu(i))
+			if (!idle_cpu(i)) {
 				cpu = i;
-			goto unlock;
+				goto unlock;
+			}
 		}
 	}
 unlock:
