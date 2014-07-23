35bad9b9b23fad50189bc2e4051f9f12544a44a7
Alfred Chen <cchalpha@gmail.com>
2014-07-23 13:57:38 +0800

    bfs: Refactory idle!=prev and next task code in schedule()
    Just refactory the code and doesn't change the logic. Reduce a dupicated
    condition sentence and some not operations.

--- kernel/sched/bfs.c
+++ kernel/sched/bfs.c
@@ -1096,20 +1096,6 @@ static inline void take_task(int cpu, struct task_struct *p)
 	dec_qnr();
 }
 
-/*
- * Returns a descheduling task to the grq runqueue unless it is being
- * deactivated.
- */
-static inline void return_task(struct task_struct *p, bool deactivate)
-{
-	if (deactivate)
-		deactivate_task(p);
-	else {
-		inc_qnr();
-		enqueue_task(p);
-	}
-}
-
 #ifndef tsk_is_polling
 #define tsk_is_polling(t) 0
 #endif
@@ -3325,28 +3311,39 @@ need_resched:
 		check_deadline(prev);
 		prev->last_ran = rq->clock_task;
 
-		/* Task changed affinity off this CPU */
-		if (needs_other_cpu(prev, cpu)) {
-			if (!deactivate)
+		if (deactivate)
+			deactivate_task(prev);
+		else {
+			/* Task changed affinity off this CPU */
+			if (unlikely(needs_other_cpu(prev, cpu)))
 				resched_suitable_idle(prev);
-		} else if (!deactivate) {
-			if (!queued_notrunning()) {
-				/*
-				* We now know prev is the only thing that is
-				* awaiting CPU so we can bypass rechecking for
-				* the earliest deadline task and just run it
-				* again.
-				*/
-				set_rq_task(rq, prev);
-				grq_unlock_irq();
-				goto rerun_prev_unlocked;
-			} else
-				swap_sticky(rq, cpu, prev);
+			else {
+				if (queued_notrunning())
+					swap_sticky(rq, cpu, prev);
+				else {
+					/*
+					* We now know prev is the only thing that is
+					* awaiting CPU so we can bypass rechecking for
+					* the earliest deadline task and just run it
+					* again.
+					*/
+					set_rq_task(rq, prev);
+					grq_unlock_irq();
+					goto rerun_prev_unlocked;
+				}
+			}
+			inc_qnr();
+			enqueue_task(prev);
 		}
-		return_task(prev, deactivate);
 	}
 
-	if (unlikely(!queued_notrunning())) {
+	if (likely(queued_notrunning())) {
+		next = earliest_deadline_task(rq, cpu, idle);
+		if (likely(next->prio != PRIO_LIMIT))
+			clear_cpuidle_map(cpu);
+		else
+			set_cpuidle_map(cpu);
+	} else {
 		/*
 		 * This CPU is now truly idle as opposed to when idle is
 		 * scheduled as a high priority task in its own right.
@@ -3354,12 +3351,6 @@ need_resched:
 		next = idle;
 		schedstat_inc(rq, sched_goidle);
 		set_cpuidle_map(cpu);
-	} else {
-		next = earliest_deadline_task(rq, cpu, idle);
-		if (likely(next->prio != PRIO_LIMIT))
-			clear_cpuidle_map(cpu);
-		else
-			set_cpuidle_map(cpu);
 	}
 
 	if (likely(prev != next)) {
