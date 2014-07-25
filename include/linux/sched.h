f2649a324b453221af1c62574640bf5936e02ead
Alfred Chen <cchalpha@gmail.com>
2014-07-25 14:27:30 +0800

    bfs: [Fix] Clean up wake_entry in task_struct

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1463,8 +1463,10 @@ struct task_struct {
 #if defined(CONFIG_SMP) || defined(CONFIG_SCHED_BFS)
 	int on_cpu;
 #endif
-#ifdef CONFIG_SMP
+#if defined(CONFIG_SMP) && !defined(CONFIG_SCHED_BFS)
 	struct llist_node wake_entry;
+#endif
+#ifdef CONFIG_SMP
 	unsigned int wakee_flips;
 	unsigned long wakee_flip_decay_ts;
 	struct task_struct *last_wakee;
