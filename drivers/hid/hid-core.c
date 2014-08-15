c05f379b79bf75ea0abb95adc9e0189050377f60
Alfred Chen <cchalpha@gmail.com>
2014-08-16 07:00:19 +0800

    hid: IBM USB ThinkPad KB handle by hid-generic.

--- drivers/hid/hid-core.c
+++ drivers/hid/hid-core.c
@@ -793,6 +793,7 @@ static int hid_scan_report(struct hid_device *hid)
 		break;
 	case USB_VENDOR_ID_SYNAPTICS:
 		if ((hid->group == HID_GROUP_GENERIC) &&
+	    	    (hid->product != USB_DEVICE_ID_SYNAPTICS_COMP_TP) &&
 		    (hid->bus != BUS_USB || hid->type == HID_TYPE_USBMOUSE))
 			/* hid-rmi should only bind to the mouse interface of
 			 * composite USB devices */
