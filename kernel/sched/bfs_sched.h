2b6321d7b55274c7b2d8ed365bd0bb277599cb63
Alfred Chen <cchalpha@gmail.com>
2014-11-03 17:13:46 +0800

    bfs: Make cpu_rq(cpu) a macro instead of a function call.

--- kernel/sched/bfs_sched.h
+++ kernel/sched/bfs_sched.h
@@ -76,8 +76,18 @@ struct rq {
 };
 
 #ifdef CONFIG_SMP
-struct rq *cpu_rq(int cpu);
-#endif
+DECLARE_PER_CPU_SHARED_ALIGNED(struct rq, runqueues);
+#define cpu_rq(cpu)		(&per_cpu(runqueues, (cpu)))
+#define this_rq()		(&__get_cpu_var(runqueues))
+#define task_rq(p)		cpu_rq(task_cpu(p))
+#define cpu_curr(cpu)		(cpu_rq(cpu)->curr)
+#else
+extern struct rq *uprq;
+#define cpu_rq(cpu)	(uprq)
+#define this_rq()	(uprq)
+#define task_rq(p)	(uprq)
+#define cpu_curr(cpu)	((uprq)->curr)
+#endif /* CONFIG_SMP */
 
 static inline u64 rq_clock(struct rq *rq)
 {
