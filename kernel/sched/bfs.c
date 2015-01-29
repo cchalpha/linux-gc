bde67355a4e5eb808098d530ff338f394b8317e7
Alfred Chen <cchalpha@gmail.com>
2015-01-29 23:04:16 +0800

    vrq: [1/6] VRQ:remove soft_affined in rq

--- kernel/sched/bfs.c
+++ kernel/sched/bfs.c
@@ -1140,7 +1140,6 @@ static void activate_task(struct task_struct *p, struct rq *rq)
 	if (task_contributes_to_load(p))
 		grq.nr_uninterruptible--;
 	enqueue_task(p, rq);
-	rq->soft_affined++;
 	p->on_rq = 1;
 	grq.nr_running++;
 	inc_qnr();
@@ -1157,7 +1156,6 @@ static inline void deactivate_task(struct task_struct *p, struct rq *rq)
 {
 	if (task_contributes_to_load(p))
 		grq.nr_uninterruptible++;
-	rq->soft_affined--;
 	p->on_rq = 0;
 	grq.nr_running--;
 	clear_sticky(p);
@@ -1194,10 +1192,6 @@ void set_task_cpu(struct task_struct *p, unsigned int cpu)
 	trace_sched_migrate_task(p, cpu);
 	perf_event_task_migrate(p);
 
-	if (p->on_rq) {
-		task_rq(p)->soft_affined--;
-		cpu_rq(cpu)->soft_affined++;
-	}
 	__set_task_cpu(p, cpu);
 }
 
@@ -2304,7 +2298,7 @@ static unsigned long nr_uninterruptible(void)
  */
 bool single_task_running(void)
 {
-	return (raw_rq()->soft_affined == 1);
+	return (raw_rq()->rq_running && (0 == queued_notrunning()));
 }
 EXPORT_SYMBOL(single_task_running);
 
@@ -2347,7 +2341,9 @@ void get_iowait_load(unsigned long *nr_waiters, unsigned long *load)
 	struct rq *rq = this_rq();
 
 	*nr_waiters = atomic_read(&rq->nr_iowait);
-	*load = rq->soft_affined;
+	/* Beyond a task running on this CPU, load is equal everywhere on BFS */
+	*load = rq->rq_running +
+		((queued_notrunning() + nr_uninterruptible()) / grq.noc);
 }
 
 /* Variables and functions for calc_load */
