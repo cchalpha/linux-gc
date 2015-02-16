ad28c2a74b57fdf87ed381a77dc1bc3bcbf2d5db
Alfred Chen <cchalpha@gmail.com>
2015-02-16 18:41:39 +0800

    bfs: [Sync] sched_getaffinity().

--- kernel/sched/bfs.c
+++ kernel/sched/bfs.c
@@ -4732,7 +4732,6 @@ long sched_getaffinity(pid_t pid, cpumask_t *mask)
 	unsigned long flags;
 	int retval;
 
-	get_online_cpus();
 	rcu_read_lock();
 
 	retval = -ESRCH;
@@ -4750,7 +4749,6 @@ long sched_getaffinity(pid_t pid, cpumask_t *mask)
 
 out_unlock:
 	rcu_read_unlock();
-	put_online_cpus();
 
 	return retval;
 }
