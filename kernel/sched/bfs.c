ae2b830b4878cad621093fc6f8f000ec62a20875
Alfred Chen <cchalpha@gmail.com>
2015-03-04 10:08:55 +0800

    bfs: vrq: grq lock free ctx switch in idle_schedule().

--- kernel/sched/bfs.c
+++ kernel/sched/bfs.c
@@ -512,6 +512,31 @@ static inline void finish_lock_switch(struct rq *rq, struct task_struct *prev)
 	raw_spin_unlock_irq(&rq->lock);
 }
 
+static inline void __finish_lock_switch(struct rq *rq, struct task_struct *prev)
+{
+#if defined(CONFIG_SMP) || defined(CONFIG_SCHED_BFS)
+	/*
+	 * After ->on_cpu is cleared, the task would be locked on grq
+	 * lock or pi_lock, We must ensure this doesn't happen until the
+	 * switch is completely finished.
+	 */
+	smp_wmb();
+	prev->on_cpu = 0;
+#endif
+#ifdef CONFIG_DEBUG_SPINLOCK
+	/* this is a valid case when another task releases the spinlock */
+	rq->lock.owner = current;
+#endif
+	/*
+	 * If we are tracking spinlock dependencies then we have to
+	 * fix up the runqueue lock - which gets 'carried over' from
+	 * prev into current:
+	 */
+	spin_acquire(&rq->lock.dep_map, 0, 0, _THIS_IP_);
+
+	raw_spin_unlock_irq(&rq->lock);
+}
+
 static inline bool deadline_before(u64 deadline, u64 time)
 {
 	return (deadline < time);
@@ -1890,7 +1915,6 @@ static struct rq *finish_task_switch(struct task_struct *prev)
 	struct rq *rq = this_rq();
 	struct mm_struct *mm = rq->prev_mm;
 	long prev_state;
-	struct rq *prq, *w_prq, *us_prq;
 
 	rq->prev_mm = NULL;
 
@@ -1909,32 +1933,11 @@ static struct rq *finish_task_switch(struct task_struct *prev)
 	vtime_task_switch(prev);
 	finish_arch_switch(prev);
 	perf_event_task_sched_in(prev, current);
-	/*
-	 * Before unlock rq, record all rq need to be reschedule in the stack
-	 */
-	if (rq->return_task) {
-		prq = task_best_idle_rq(rq->return_task);
-		rq->return_task = NULL;
-	} else
-		prq = NULL;
-	if (rq->wakeup_worker) {
-		if (current != rq->wakeup_worker)
-			w_prq = task_best_idle_rq(rq->wakeup_worker);
-		else
-			w_prq = NULL;
-		rq->wakeup_worker = NULL;
-	} else
-		w_prq = NULL;
-	if (rq->unsticky_task) {
-		if (task_queued(rq->unsticky_task))
-			us_prq = task_best_idle_rq(rq->unsticky_task);
-		else
-			us_prq = NULL;
-		rq->unsticky_task = NULL;
-	} else
-		us_prq = NULL;
 
-	finish_lock_switch(rq, prev);
+	if (unlikely(prev == task_rq(prev)->idle))
+		__finish_lock_switch(rq, prev);
+	else
+		finish_lock_switch(rq, prev);
 	finish_arch_post_lock_switch();
 
 	fire_sched_in_preempt_notifiers(current);
@@ -1948,12 +1951,6 @@ static struct rq *finish_task_switch(struct task_struct *prev)
 		kprobe_flush_task(prev);
 		put_task_struct(prev);
 	}
-
-	preempt_rq(prq);
-	if (w_prq != prq)
-		preempt_rq(w_prq);
-	if (us_prq != prq && us_prq != w_prq)
-		preempt_rq(us_prq);
 
 	return rq;
 }
@@ -1984,6 +1981,7 @@ context_switch(struct rq *rq, struct task_struct *prev,
 	       struct task_struct *next)
 {
 	struct mm_struct *mm, *oldmm;
+	struct rq *prq, *w_prq, *us_prq;
 
 	prepare_task_switch(rq, prev, next);
 
@@ -2013,7 +2011,8 @@ context_switch(struct rq *rq, struct task_struct *prev,
 	 * of the scheduler it's an obvious special-case), so we
 	 * do an early lockdep release here:
 	 */
-	spin_release(&grq.lock.dep_map, 1, _THIS_IP_);
+	if (prev != rq->idle)
+		spin_release(&grq.lock.dep_map, 1, _THIS_IP_);
 	spin_release(&rq->lock.dep_map, 1, _THIS_IP_);
 
 	/* Here we just switch the register state and the stack. */
@@ -2022,7 +2021,40 @@ context_switch(struct rq *rq, struct task_struct *prev,
 
 	barrier();
 
-	return finish_task_switch(prev);
+	/*
+	 * Before unlock rq, record all rq need to be reschedule in the stack
+	 */
+	if (rq->return_task) {
+		prq = task_best_idle_rq(rq->return_task);
+		rq->return_task = NULL;
+	} else
+		prq = NULL;
+	if (rq->wakeup_worker) {
+		if (current != rq->wakeup_worker)
+			w_prq = task_best_idle_rq(rq->wakeup_worker);
+		else
+			w_prq = NULL;
+		rq->wakeup_worker = NULL;
+	} else
+		w_prq = NULL;
+	if (rq->unsticky_task) {
+		if (task_queued(rq->unsticky_task))
+			us_prq = task_best_idle_rq(rq->unsticky_task);
+		else
+			us_prq = NULL;
+		rq->unsticky_task = NULL;
+	} else
+		us_prq = NULL;
+
+	rq = finish_task_switch(prev);
+
+	preempt_rq(prq);
+	if (w_prq != prq)
+		preempt_rq(w_prq);
+	if (us_prq != prq && us_prq != w_prq)
+		preempt_rq(us_prq);
+
+	return rq;
 }
 
 /*
@@ -3401,16 +3433,20 @@ static inline void idle_schedule(int cpu, struct rq *rq, struct task_struct *pre
 	update_rq_clock(rq);
 	update_cpu_clock_switch_idle(rq, prev);
 	rq->dither = (rq->clock - rq->last_tick < HALF_JIFFY_NS);
-	_grq_lock();
 
 	bfs_stat_idle++;
 	if (likely(queued_notrunning())) {
 		struct task_struct *next;
 
 		bfs_stat_idle_qnr++;
+
+		_grq_lock();
 		next = earliest_deadline_task(rq, cpu, prev);
 
 		if (likely(prev != next)) {
+			grq.nr_switches++;
+			_grq_unlock();
+
 			if (likely(next->prio != PRIO_LIMIT))
 				clear_cpuidle_map(cpu);
 			else
@@ -3420,26 +3456,30 @@ static inline void idle_schedule(int cpu, struct rq *rq, struct task_struct *pre
 
 			check_smt_siblings(cpu);
 
-			grq.nr_switches++;
 			rq->curr = next;
 			++*switch_count;
 
-			rq = context_switch(rq, prev, next); /* unlocks the grq */
+			rq = context_switch(rq, prev, next); /* unlocks the rq */
 
 			return;
 		}
+		_grq_unlock();
+
 		bfs_stat_idle_qnr_eq++;
-	} else {
-		/*
-		 * This CPU is now truly idle as opposed to when idle is
-		 * scheduled as a high priority task in its own right.
-		 * idle -> idle should be avoid, inc counter to trace this
-		 */
-		schedstat_inc(rq, sched_goidle);
+
+		check_smt_siblings(cpu);
+		raw_spin_unlock_irq(&rq->lock);
+
+		return;
 	}
+	/*
+	 * This CPU is now truly idle as opposed to when idle is
+	 * scheduled as a high priority task in its own right.
+	 * idle -> idle should be avoid, inc counter to trace this
+	 */
+	schedstat_inc(rq, sched_goidle);
 
 	check_smt_siblings(cpu);
-	_grq_unlock();
 	raw_spin_unlock_irq(&rq->lock);
 }
 
