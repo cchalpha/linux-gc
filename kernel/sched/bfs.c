a0c0129abc8dafbaaade119e94afed9721e4b483
Alfred Chen <cchalpha@gmail.com>
2015-04-21 23:52:47 +0800

    bfs: vrq, refactory wake_up_new_task.

--- kernel/sched/bfs.c
+++ kernel/sched/bfs.c
@@ -1704,26 +1704,26 @@ void wake_up_new_task(struct task_struct *p)
 	struct task_struct *parent;
 	unsigned long flags;
 	struct rq *rq, *prq = NULL;
-	raw_spinlock_t *lock;
 
 	parent = p->parent;
-	/* p is not running and not queued, task_vrq_lock...() will
-	 * will lock on both rq and grq
+	/*
+	 * p is new task, the only reference is right here, this_cpu() is one
+	 * p allowed to run, lock on this_rq to avoid waiting for other rq
 	 */
-	rq = task_access_lock_irqsave(p, &lock, &flags);
+retry:
+	rq = this_rq();
+	raw_spin_lock_irqsave(&rq->lock, flags);
+	if (unlikely(rq != this_rq())) {
+		raw_spin_unlock_irqrestore(&rq->lock, flags);
+		goto retry;
+	}
 
+	set_task_cpu(p, rq->cpu);
 	/*
 	 * Reinit new task deadline as its creator deadline could have changed
 	 * since call to dup_task_struct().
 	 */
 	p->deadline = rq->rq_deadline;
-
-	/*
-	 * If the task is a new process, current and parent are the same. If
-	 * the task is a new thread in the thread group, it will have much more
-	 * in common with current than with the parent.
-	 */
-	set_task_cpu(p, task_cpu(rq->curr));
 
 	/*
 	 * Make sure we do not leak PI boosting priority to the child.
@@ -1732,7 +1732,6 @@ void wake_up_new_task(struct task_struct *p)
 
 	update_task_priodl(p);
 
-	activate_task(p, rq);
 	trace_sched_wakeup_new(p, 1);
 	if (unlikely(p->policy == SCHED_FIFO))
 		goto after_ts_init;
@@ -1758,8 +1757,9 @@ after_ts_init:
 			 * usually avoids a lot of COW overhead.
 			 */
 			__set_tsk_resched(parent);
-		} else
+		} else {
 			prq = task_preemptable_rq(p);
+		}
 	} else {
 		if (rq->curr == parent) {
 			/*
@@ -1773,7 +1773,12 @@ after_ts_init:
 		}
 		time_slice_expired(p, rq);
 	}
-	task_access_unlock_irqrestore(lock, &flags);
+
+	_grq_lock();
+	activate_task(p, rq);
+	_grq_unlock();
+
+	raw_spin_unlock_irqrestore(&rq->lock, flags);
 
 	preempt_rq(prq);
 }
