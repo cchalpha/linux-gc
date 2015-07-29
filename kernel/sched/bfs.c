7ac0b9bf7fcd6ff3ca07f2958d2e3ec8a1cd4fd1
Alfred Chen <cchalpha@gmail.com>
2015-07-29 10:59:31 +0800

    bfs: [Sync] sched_init_numa().

--- kernel/sched/bfs.c
+++ kernel/sched/bfs.c
@@ -6458,6 +6458,10 @@ static void sched_init_numa(void)
 		if (!sched_debug())
 			break;
 	}
+
+	if (!level)
+		return;
+
 	/*
 	 * 'level' contains the number of unique distances, excluding the
 	 * identity distance node_distance(i,i).
