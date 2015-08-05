c49c13c25d2e91f149f4f90b0f2baa466bed7159
Alfred Chen <cchalpha@gmail.com>
2015-08-05 10:52:05 +0800

    bfs: -gc BFS enchancement patch set version.
    Add -gc patch set version in dmesg.

--- kernel/sched/bfs.c
+++ kernel/sched/bfs.c
@@ -132,6 +132,7 @@
 void print_scheduler_version(void)
 {
 	printk(KERN_INFO "BFS CPU scheduler v0.472 by Con Kolivas.\n");
+	printk(KERN_INFO "BFS enhancement patchset v4.7_0472_0 by Alfred Chen.\n");
 }
 
 /*
