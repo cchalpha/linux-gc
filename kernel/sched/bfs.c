09da3bbd8c4422b8def94838fdc6349c0cfaa926
Alfred Chen <cchalpha@gmail.com>
2015-09-04 10:47:02 +0800

    bfs: [Fix] Add additional checking in sched_submit_work()
    When sched_submit_works() is introduced in mainline 3.1, it moves the
    blk_schedule_flush_plug(tsk) call outside from schedule(), but relaxing
    the checking when not calling it. The code change is ok for mainline
    CFS but it's not for BFS somehow.
    
    Adding back those checking is the current solution for BFS.

--- kernel/sched/bfs.c
+++ kernel/sched/bfs.c
@@ -3580,7 +3580,9 @@ rerun_prev_unlocked:
 
 static inline void sched_submit_work(struct task_struct *tsk)
 {
-	if (!tsk->state || tsk_is_pi_blocked(tsk))
+	if (!tsk->state || tsk_is_pi_blocked(tsk)
+		|| (preempt_count() & PREEMPT_ACTIVE)
+		|| signal_pending_state(tsk->state, tsk))
 		return;
 	/*
 	 * If we are going to sleep and we have plugged IO queued,
