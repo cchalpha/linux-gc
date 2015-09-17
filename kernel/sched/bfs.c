0a0d1caac26fa9b7b07c81c9f40533a52040ce3c
Alfred Chen <cchalpha@gmail.com>
2015-09-17 10:34:43 +0800

    bfs/vrq: -vrq BFS enchancement patch set version

--- kernel/sched/bfs.c
+++ kernel/sched/bfs.c
@@ -139,7 +139,7 @@
 void print_scheduler_version(void)
 {
 	printk(KERN_INFO "BFS CPU scheduler v0.465 by Con Kolivas.\n");
-	printk(KERN_INFO "BFS enhancement patchset v4.3_0465_3 by Alfred Chen.\n");
+	printk(KERN_INFO "BFS enhancement patchset v4.3_0465_2_vrq1 by Alfred Chen.\n");
 }
 
 /* BFS default rr interval in ms */
