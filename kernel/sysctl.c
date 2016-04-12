c62e165bbac3fe698380665d9f366686b3b9bb97
Alfred Chen <cchalpha@gmail.com>
2016-04-12 14:17:36 +0800

    bfs/vrq: Remove BFS sched_interactive

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -129,7 +129,6 @@ static int __read_mostly one_hundred = 100;
 static int __read_mostly one_thousand = 1000;
 #ifdef CONFIG_SCHED_BFS
 extern int rr_interval;
-extern int sched_interactive;
 extern int sched_iso_cpu;
 #endif
 #ifdef CONFIG_PRINTK
@@ -1019,15 +1018,6 @@ static struct ctl_table kern_table[] = {
 		.proc_handler	= &proc_dointvec_minmax,
 		.extra1		= &one,
 		.extra2		= &one_thousand,
-	},
-	{
-		.procname	= "interactive",
-		.data		= &sched_interactive,
-		.maxlen		= sizeof(int),
-		.mode		= 0644,
-		.proc_handler	= &proc_dointvec_minmax,
-		.extra1		= &zero,
-		.extra2		= &one,
 	},
 	{
 		.procname	= "iso_cpu",
