6a4fd451c7104a7c001b06e60a44e7e2e327e2ff
Alfred Chen <cchalpha@gmail.com>
2016-09-03 09:57:41 +0800

    bfs: bfs472-skiplist.patch

--- include/linux/init_task.h
+++ include/linux/init_task.h
@@ -204,7 +204,7 @@ extern struct task_group root_task_group;
 	.restart_block = {						\
 		.fn = do_no_restart_syscall,				\
 	},								\
-	.run_list	= LIST_HEAD_INIT(tsk.run_list),			\
+	.node		= NULL,						\
 	.time_slice	= HZ,					\
 	.tasks		= LIST_HEAD_INIT(tsk.tasks),			\
 	INIT_PUSHABLE_TASKS(tsk)					\
