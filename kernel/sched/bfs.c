ebd1bd3c3f9956ad26f48a558e7c1dcef14ac56e
Alfred Chen <cchalpha@gmail.com>
2016-09-18 12:32:52 +0800

    bfs: [Sync] sched_setscheduler() logic, v6

--- kernel/sched/bfs.c
+++ kernel/sched/bfs.c
@@ -4348,9 +4348,6 @@ static void __setscheduler_params(struct task_struct *p,
 static void __setscheduler(struct rq *rq, struct task_struct *p,
 			   const struct sched_attr *attr, bool keep_boost)
 {
-	int oldrtprio = p->rt_priority;
-	int oldprio = p->prio;
-
 	__setscheduler_params(p, attr);
 
 	/*
@@ -4362,13 +4359,6 @@ static void __setscheduler(struct rq *rq, struct task_struct *p,
 	else
 		p->prio = p->normal_prio;
 	update_task_priodl(p);
-
-	if (task_running(p)) {
-		reset_rq_task(rq, p);
-		/* Resched only if we might now be preempted */
-		if (p->prio > oldprio || p->rt_priority > oldrtprio)
-			resched_curr(rq);
-	}
 }
 
 /*
