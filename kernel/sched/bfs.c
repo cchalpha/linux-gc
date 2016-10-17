637dc20f300451b101448321fb1842aca174afdc
Alfred Chen <cchalpha@gmail.com>
2016-10-17 22:44:35 +0800

    bfs/vrq: add lockdep in enqueue/dequeue_task()

--- kernel/sched/bfs.c
+++ kernel/sched/bfs.c
@@ -557,6 +557,7 @@ static inline void grq_priodl_unlock(void)
 static void dequeue_task(struct task_struct *p, struct rq *rq)
 {
 	lockdep_assert_held(&grq.lock);
+	lockdep_assert_held(&rq->lock);
 
 	skiplist_del_init(&grq.sl_header, &p->sl_node);
 
@@ -636,6 +637,7 @@ DEFINE_SKIPLIST_INSERT_FUNC(bfs_skiplist_insert, bfs_skiplist_task_search);
 static void enqueue_task(struct task_struct *p, struct rq *rq)
 {
 	lockdep_assert_held(&grq.lock);
+	lockdep_assert_held(&rq->lock);
 
 	if (!rt_task(p)) {
 		/* Check it hasn't gotten rt from PI */
