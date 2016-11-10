66a6b61cd6860e06257e8a5ef04e62ab98dcc0dc
Alfred Chen <cchalpha@gmail.com>
2016-11-10 10:09:24 +0800

    vrq: Tag VRQ 0.89a

--- kernel/sched/bfs.c
+++ kernel/sched/bfs.c
@@ -134,7 +134,7 @@
 void print_scheduler_version(void)
 {
 	printk(KERN_INFO "BFS CPU scheduler v0.472 by Con Kolivas.\n");
-	printk(KERN_INFO "BFS enhancement patchset VRQ 0.89 by Alfred Chen.\n");
+	printk(KERN_INFO "BFS enhancement patchset VRQ 0.89a by Alfred Chen.\n");
 }
 
 /* task_struct::on_rq states: */
