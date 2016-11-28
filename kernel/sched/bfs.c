345d209b7857e7bf5e95b5a109a99a9c672afea7
Alfred Chen <cchalpha@gmail.com>
2016-11-28 12:26:51 +0800

    vrq: c-state issue fix.

--- kernel/sched/bfs.c
+++ kernel/sched/bfs.c
@@ -2555,7 +2555,7 @@ void get_iowait_load(unsigned long *nr_waiters, unsigned long *load)
 	struct rq *rq = this_rq();
 
 	*nr_waiters = atomic_read(&rq->nr_iowait);
-	*load = rq->nr_running + rq->nr_uninterruptible;
+	*load = rq->nr_running;
 }
 
 /* Variables and functions for calc_load */
