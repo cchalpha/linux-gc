b10f47a322f0e36fefe9b20da5447f3ba8fa0766
Alfred Chen <cchalpha@gmail.com>
2016-12-14 11:19:29 +0800

    vrq: [Sync] 9dcb8b6 mm: remove per-zone hashtable of bitlock waitqueues

--- kernel/sched/bfs.c
+++ kernel/sched/bfs.c
@@ -7271,6 +7271,19 @@ int in_sched_functions(unsigned long addr)
 		&& addr < (unsigned long)__sched_text_end);
 }
 
+#define WAIT_TABLE_BITS 8
+#define WAIT_TABLE_SIZE (1 << WAIT_TABLE_BITS)
+static wait_queue_head_t bit_wait_table[WAIT_TABLE_SIZE] __cacheline_aligned;
+
+wait_queue_head_t *bit_waitqueue(void *word, int bit)
+{
+	const int shift = BITS_PER_LONG == 32 ? 5 : 6;
+	unsigned long val = (unsigned long)word << shift | bit;
+
+	return bit_wait_table + hash_long(val, WAIT_TABLE_BITS);
+}
+EXPORT_SYMBOL(bit_waitqueue);
+
 void __init sched_init(void)
 {
 #ifdef CONFIG_SMP
@@ -7278,6 +7291,9 @@ void __init sched_init(void)
 #endif
 	int i;
 	struct rq *rq;
+
+	for (i = 0; i < WAIT_TABLE_SIZE; i++)
+		init_waitqueue_head(bit_wait_table + i);
 
 	prio_ratios[0] = 128;
 	for (i = 1 ; i < NICE_WIDTH ; i++)
