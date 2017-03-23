4b722d521712a46bbee6e4b26c081a762fd38b75
Alfred Chen <cchalpha@gmail.com>
2017-03-23 16:35:25 +0800

    Tag VRQ 0.94

--- kernel/sched/bfs.c
+++ kernel/sched/bfs.c
@@ -139,7 +139,7 @@ enum {
 
 void print_scheduler_version(void)
 {
-	printk(KERN_INFO "BFS enhancement patchset VRQ 0.93a by Alfred Chen.\n");
+	printk(KERN_INFO "BFS enhancement patchset VRQ 0.94 by Alfred Chen.\n");
 }
 
 /* task_struct::on_rq states: */
